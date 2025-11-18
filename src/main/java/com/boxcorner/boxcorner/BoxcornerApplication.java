78f4d9da32712476b043f507a31c1876ad9fb051
Panu <sphanu9400@gmail.com>
2025-11-18 16:34:24 +0700

    ✨
    ✨

---
+++ src/main/java/com/boxcorner/boxcorner/BoxcornerApplication.java
@@ -0,0 +1,13 @@
+package com.boxcorner.boxcorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxcornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxcornerApplication.class, args);
+	}
+
+}
